4236b91ceeebcd482e837eb813092d97d04a8a0f
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-10-27 01:24:00 +0900

    add example

---
+++ example.py
@@ -0,0 +1,15 @@
+from zaim_pay_importer import ZaimPayImporter
+
+if __name__ == "__main__":
+    CONSUMER_ID = "xxxxxxxxxx"
+    CONSUMER_SECRET = "xxxxxxxxxx"
+    ACCESS_TOKEN = "xxxxxxxxxx"
+    ACCESS_TOKEN_SECRET = "xxxxxxxxxx"
+    CREDENTIALS_PATH = "credentials.json"
+    EXCLUDE_SET = {"amazon", "チャージ", "チヤージ", "charge", "jr east mobile suica"}
+
+    client = ZaimPayImporter(
+        CONSUMER_ID, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CREDENTIALS_PATH, EXCLUDE_SET
+    )
+    client.import_ana_pay()
+    client.import_rakuten_pay()
